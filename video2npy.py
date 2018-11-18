6fd40db24cc2724d2700f80b1de343c4a6101e1a
leaf <tracy_leaf@126.com>
2018-11-19 00:05:25 +0800

    normalize rgb .npy [-1,1]

--- video2npy.py
+++ video2npy.py
@@ -7,20 +7,24 @@ frameWidth = 224 #int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)/4)
 frameHeight = 224 #int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)/4)
 frameCount = 30
 
-buf = np.empty((1, frameCount, frameHeight, frameWidth, 3), np.dtype('uint8'))
+buf = np.empty((1, frameCount, frameHeight, frameWidth, 3), np.dtype('float32'))#uint8
 
 fc = 0
 ret = True
 
 while (fc < frameCount and ret):
     ret, frame = cap.read()
-    frame = cv2.resize(frame,(frameWidth, frameHeight), None)#, 0, 0, cv2.INTER_LINEAR)
+    frame = cv2.resize(frame,(frameWidth, frameHeight), None , 0, 0, cv2.INTER_LINEAR)
+    # print(frame)
     max_val = lambda x: max(max(x.flatten()), abs(min(x.flatten()))) #rescale(-1,1)
-    # print(max_val(frame))
-    buf[0][fc] = frame / max_val(frame)
+    # normalizedImg = np.zeros((224, 224))
+    # normalizedImg = cv2.normalize(frame, normalizedImg, alpha=-1, beta=1.0, norm_type=cv2.NORM_MINMAX)
+    # buf[0][fc] = normalizedImg
+    buf[0][fc] = (frame / float(max_val(frame))) * 2 -1
+    print(min((buf[0][fc]).flatten()))
     # buf[0][fc] = frame / 255.
-    # print(max_val(buf[0][fc]))
     fc += 1
+
 
 cap.release()
 np.save('v_Biking_g06_c02',buf) #24881317_23_part_6_rgb
